6e61ec693507325777de99283b61de7371813c27
SymYuk <g1527043@st.t-kougei.ac.jp>
2017-01-10 15:52:07 +0900

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,47 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	//DBに接続
+	var Connection=require('tedious'),Connection;
+	var config={
+		userName: 'symyuk',
+		password: 'UraNyan1000',
+		server:'2016web2-Sym',
+		options: {encrypt: true, database: '2016web2-13'}
+	}
+	var connection=new Connection(config);
+	connection.on('connect',function(err){
+		if(err){
+ 			 res.render('index', { title:"はｚっぜぜいじめてんとBD",message:err });
+		}else{
+			console.log("Connected");
+			executeStatement();
+		}
 });
+	
+var Request=require('tedious').Request;
+var TYPES=require('tedious').TYPES;
+	
+function executeStatement(){
+	request=new Request("SELECT TOP(10) CompanyName FROM SalesLT.Customer;",function(err){
+		if(err){
+			console.log(err);}
+	});
+	
+	var result='<table>';
+	
+	request.on('row',function(columns){//データの取得毎に呼ぶ
+		result+='<tr>';
+		colmns.forEach(function(column){
+			if(column.value===null){
+				console.log('NULL');
+			}else{
+				result+='<td>'+column.value+'</td>';
+			}
+			result+='</tr>';
+		});
+	});
+	
+	request.on
 
 module.exports = router;
